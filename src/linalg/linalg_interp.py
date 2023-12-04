348b5d1764b9d86313d29480c17587fad7f71531
egrozell <ethan.grozell@ucalgary.ca>
2023-12-04 12:27:18 -0800

    completed linalg function guass iter solve started spline function

--- src/linalg/linalg_interp.py
+++ src/linalg/linalg_interp.py
@@ -36,7 +36,72 @@ def gauss_iter_solve(A,b,x0=None,tol,alg= 'seidel'):
     if not (dimb := len(b.shape)) == 2 or 1:
         raise ValueError("b's is {dimb} dimension but needs to be 1D or 2D")
 
+    n_b = b.shape[0]
+    if rs_1d := (dimb == 1):
+        b = np.reshape(b, (nb,mb :=1))
+    else: mb=b.shape[1]
 
-    pass
-def spline_funtion(xd,yd,order):
+    if(mb := b.shape[0]) != n:
+        raise ValueError("b has {mb} # of rows which should be the same # of rows from A")
+
+    if x0 is None:
+        x0 = np.zeros(n).reshape(-1,1)
+    if len(x0.shape)==1:
+        x0 = np.reshape(x0,(n,1))
+    if not rs_1d and x0.shape[1]==1:
+        x0 = x0 @ np.ones((1,mb))
+
+    alg= alg.strip().lower()
+
+    eps_a = 1
+    eps_s = tol
+    i = 1
+    max_i = 100
+
+    Adiag = np.diag(np.diagA))
+    Adiaginv = np.linalg.inv(Adiag)
+    A0 = A - Adiag
+    A0st = Adiaginv @ A0
+    bst = Adiaginv @ b
+
+    if alg == 'jacobi':
+        while np.max(eps_a) > eps_s and i < max_i:
+            x_last = np.array(x0)
+            x0 = bst - (A0st @ x0)
+            i += 1
+            delta_x = x0 - x_last
+            eps_a = np.linalg.norm(delta_x, axis =0) / np.linalg.norm(x0, axis = 0)
+            if i > max_i:
+                raise RuntimeWarning('system not converging')
+    elif alg == 'seidel':
+        while np.max(eps_a) > eps_s and i < max_i:
+           x_last = np.array(x0)
+           for i, _ in enumerate(A):
+               x0[i,:] = bst[i:i+1,:] - A0st[i:i+1,:] @ x0[:,:]
+           i += 1
+           delta_x = x0 - x_last
+           eps_a = np.linalg.norm(delta_x, axis =0) / np.linalg.norm(x0, axis = 0)
+           if i > max_i:
+               raise RuntimeWarning('system not converging')
+
+    else:
+        raise ValueError('alg must be either jacobi or seidel')
+    return x0
+
+def spline_funtion(xd,yd,order = 3):
+    """
+    Generates a spline function given two vectors x and y
+
+    Parameters
+    -------------------------------------------------------
+    xd : array like float [data with increasing value]
+
+    yd : array_like float []
+
+    order : int [default 3 can be 1,2, or 3]
+
+    Returns
+    -------------------------------------------------------
+
+    """
     pass
